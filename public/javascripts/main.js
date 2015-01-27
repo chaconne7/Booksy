65aac3c9b4c9a592e8897fbbdbe3f0491a66989a
chaconne7 <whitekeyblackkey@comcast.net>
2015-01-27 15:28:58 -0500

    First commit

---
+++ public/javascripts/main.js
@@ -0,0 +1,136 @@
+
+document.getElementsByClassName("link")[0].addEventListener("click", getFeaturesPage);
+document.getElementsByClassName("link")[1].addEventListener("click", getBooksPage);
+document.getElementsByClassName("link")[2].addEventListener("click", getContactPage);
+
+document.getElementsByClassName("book")[0].addEventListener("click", getTomSawyerPage);
+document.getElementsByClassName("book")[1].addEventListener("click", getPridePrejudicePage);
+document.getElementsByClassName("book")[2].addEventListener("click", getTreasureIslandPage);
+document.getElementsByClassName("book")[3].addEventListener("click", getTimeMachinePage);
+document.getElementsByClassName("book")[4].addEventListener("click", getInvisibleManPage);
+
+
+
+function getFeaturesPage(){
+	document.getElementsByClassName("landingpage")[0].style.display="none";
+	document.getElementsByClassName("contact")[0].style.display="none";
+	document.getElementsByClassName("books")[0].style.display="none";
+	document.getElementsByClassName("tomSawyerPage")[0].style.display="none";
+	document.getElementsByClassName("pridePrejudicePage")[0].style.display="none";
+	document.getElementsByClassName("treasureIslandPage")[0].style.display="none";
+	document.getElementsByClassName("timeMachinePage")[0].style.display="none";
+	document.getElementsByClassName("invisibleManPage")[0].style.display="none";
+	
+	document.getElementsByClassName("features")[0].style.display="inline";
+
+
+}
+
+function getBooksPage(){
+	document.getElementsByClassName("landingpage")[0].style.display="none";
+	document.getElementsByClassName("contact")[0].style.display="none";
+	document.getElementsByClassName("features")[0].style.display="none";
+	document.getElementsByClassName("tomSawyerPage")[0].style.display="none";
+	document.getElementsByClassName("pridePrejudicePage")[0].style.display="none";
+	document.getElementsByClassName("treasureIslandPage")[0].style.display="none";
+	document.getElementsByClassName("timeMachinePage")[0].style.display="none";
+	document.getElementsByClassName("invisibleManPage")[0].style.display="none";
+	
+	document.getElementsByClassName("books")[0].style.display="inline";
+
+
+}
+
+function getContactPage(){
+	document.getElementsByClassName("landingpage")[0].style.display="none";
+	document.getElementsByClassName("books")[0].style.display="none";
+	document.getElementsByClassName("features")[0].style.display="none";
+	document.getElementsByClassName("tomSawyerPage")[0].style.display="none";
+	document.getElementsByClassName("pridePrejudicePage")[0].style.display="none";
+	document.getElementsByClassName("treasureIslandPage")[0].style.display="none";
+	document.getElementsByClassName("timeMachinePage")[0].style.display="none";
+	document.getElementsByClassName("invisibleManPage")[0].style.display="none";
+	
+	document.getElementsByClassName("contact")[0].style.display="inline";
+
+
+}
+
+function getTomSawyerPage(){
+	document.getElementsByClassName("landingpage")[0].style.display="none";
+	document.getElementsByClassName("contact")[0].style.display="none";
+	document.getElementsByClassName("features")[0].style.display="none";
+	document.getElementsByClassName("books")[0].style.display="none";
+	document.getElementsByClassName("pridePrejudicePage")[0].style.display="none";
+	document.getElementsByClassName("treasureIslandPage")[0].style.display="none";
+	document.getElementsByClassName("timeMachinePage")[0].style.display="none";
+	document.getElementsByClassName("invisibleManPage")[0].style.display="none";
+
+	document.getElementsByClassName("tomSawyerPage")[0].style.display="inline";
+
+
+}
+
+function getPridePrejudicePage(){
+	document.getElementsByClassName("landingpage")[0].style.display="none";
+	document.getElementsByClassName("contact")[0].style.display="none";
+	document.getElementsByClassName("books")[0].style.display="none";
+	document.getElementsByClassName("features")[0].style.display="none";
+	document.getElementsByClassName("tomSawyerPage")[0].style.display="none";
+	document.getElementsByClassName("treasureIslandPage")[0].style.display="none";
+	document.getElementsByClassName("timeMachinePage")[0].style.display="none";
+	document.getElementsByClassName("invisibleManPage")[0].style.display="none";
+	
+	document.getElementsByClassName("pridePrejudicePage")[0].style.display="inline";
+
+
+}
+
+
+function getTreasureIslandPage(){
+	document.getElementsByClassName("landingpage")[0].style.display="none";
+	document.getElementsByClassName("contact")[0].style.display="none";
+	document.getElementsByClassName("books")[0].style.display="none";
+	document.getElementsByClassName("features")[0].style.display="none";
+	document.getElementsByClassName("tomSawyerPage")[0].style.display="none";
+	document.getElementsByClassName("pridePrejudicePage")[0].style.display="none";
+	document.getElementsByClassName("timeMachinePage")[0].style.display="none";
+	document.getElementsByClassName("invisibleManPage")[0].style.display="none";
+	
+	document.getElementsByClassName("treasureIslandPage")[0].style.display="inline";
+
+
+}
+
+function getTimeMachinePage(){
+	document.getElementsByClassName("landingpage")[0].style.display="none";
+	document.getElementsByClassName("contact")[0].style.display="none";
+	document.getElementsByClassName("books")[0].style.display="none";
+	document.getElementsByClassName("features")[0].style.display="none";
+	document.getElementsByClassName("treasureIslandPage")[0].style.display="none";
+	document.getElementsByClassName("invisibleManPage")[0].style.display="none";
+	document.getElementsByClassName("tomSawyerPage")[0].style.display="none";
+	document.getElementsByClassName("pridePrejudicePage")[0].style.display="none";
+	
+	document.getElementsByClassName("timeMachinePage")[0].style.display="inline";
+
+
+}
+
+function getInvisibleManPage(){
+	document.getElementsByClassName("landingpage")[0].style.display="none";
+	document.getElementsByClassName("contact")[0].style.display="none";
+	document.getElementsByClassName("books")[0].style.display="none";
+	document.getElementsByClassName("features")[0].style.display="none";
+	document.getElementsByClassName("tomSawyerPage")[0].style.display="none";
+	document.getElementsByClassName("pridePrejudicePage")[0].style.display="none";
+	document.getElementsByClassName("timeMachinePage")[0].style.display="none";
+	document.getElementsByClassName("treasureIslanPage")[0].style.display="none";
+	
+	document.getElementsByClassName("invisibleManPage")[0].style.display="inline";
+
+
+}
+
+
+
